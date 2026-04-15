8760637d09fe8f847df6b53da54889699db95e07
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-04-15 14:04:34 -0400

    agregar GlobalExceptionHandler y correccion de validaciones en model

---
+++ src/main/java/com/jara/antonio/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.jara.antonio.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> manejoErroresValidacion(MethodArgumentNotValidException ex) {
+        @SuppressWarnings("unused")
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errores.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return errores;
+
+    }
+
+}
--- src/main/java/com/jara/antonio/model/Evento.java
+++ src/main/java/com/jara/antonio/model/Evento.java
@@ -1,7 +1,5 @@
 package com.jara.antonio.model;
 
-import java.time.LocalDate;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -12,14 +10,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Evento {
-    @NotNull(message = "El ID del evento no puede estar vacío.")
-    private int id;
-    @NotBlank(message = "El nombre del evento no puede estar vacío.")
+    @NotNull(message = "El ID del evento no puede ser nulo.")
+    // cambio de int al wrapper Integer para hacer uso de la validacion @NotNull
+    private Integer id;
+    @NotBlank(message = "El nombre no puede ser vacío.")
     private String nombre;
+    @NotBlank(message = "El tipo no puede ser vacío.")
     private String tipo;
-    @NotNull(message = "La fecha del evento no puede estar vacía.")
-    private LocalDate fecha;
-    @NotBlank(message = "La ubicación del evento no puede estar vacía.")
+    @NotNull(message = "La fecha no puede ser vacía.")
+    private String fecha;
+    @NotBlank(message = "La ubicación no puede ser vacía.")
     private String ubicacion;
-    private int asistentes;
+    @NotNull(message = "La capacidad no puede ser nula.")
+    private Integer capacidad;
 }
