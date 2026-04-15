5960d433cbab5d4f4881f66a27cbeb5315fca429
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-04-15 14:10:11 -0400

    completar repository

--- src/main/java/com/jara/antonio/repository/EventoRepository.java
+++ src/main/java/com/jara/antonio/repository/EventoRepository.java
@@ -26,4 +26,31 @@ public class EventoRepository {
         }
         return null;
     }
+
+    public Evento actualizar(Integer id, Evento evento) {
+        Evento buscado = obtenerPorId(id);
+        if (buscado != null){
+            buscado.setNombre(evento.getNombre());
+            buscado.setTipo(evento.getTipo());
+            buscado.setFecha(evento.getFecha());
+            buscado.setUbicacion(evento.getUbicacion());
+            buscado.setCapacidad(evento.getCapacidad());
+        }
+        return null;
+    }
+
+    public boolean eliminar(Integer id){
+        return listaEventos.removeIf(e -> e.getId().equals(id));
+    }
+
+    // operacion de trasnformacion solicitada
+    public List<Evento> buscarPorTipo(String tipo) {
+        List<Evento> filtrados = new ArrayList<>();
+        for (Evento evento : listaEventos) {
+            if (evento.getTipo().equalsIgnoreCase(tipo)) {
+                filtrados.add(evento);
+            }
+        }
+        return filtrados;
+    }
 }  
